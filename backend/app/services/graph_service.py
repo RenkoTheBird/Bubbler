1c422e575754e04dd8308a73203195579f739e70
RenkoTheBird <raindrop.spider@protonmail.com>
2026-05-26 14:03:43 -0400

    Clarify graph service

---
+++ backend/app/services/graph_service.py
@@ -0,0 +1,9 @@
+from backend.app.main import app
+
+'''
+TODO: GOAL: Based upon current post, get 4 posts with the most similarity
+In other words: user picks post (feed, graph) -> 4 posts shown -> user picks 1 of these -> repeat
+So this will be called every time that happens
+
+Since we are just using similarity for now, use @similarity_service.py
+'''

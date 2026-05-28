334af8d862524218e081dfbcebc048de03f099f2
RenkoTheBird <raindrop.spider@protonmail.com>
2026-05-28 16:02:27 -0400

    Add user routing

--- backend/app/api/routes/users.py
+++ backend/app/api/routes/users.py
@@ -1,4 +1,12 @@
 from fastapi import APIRouter
+from fastapi import Depends
 
 router = APIRouter()
 
+@router.get("users/{id}/profile")
+def getProfileInfo(service: UserService = Depends()):
+    return service.getProfileInfo()
+
+@router.put("users/{id}/profile/email")
+def putEmail(service: UserService = Depends()):
+    return service.putEmail()

65c4b87250da49d09154282052332c4a72d3be12
RenkoTheBird <raindrop.spider@protonmail.com>
2026-06-01 13:21:34 -0400

    Add interaction service

---
+++ backend/app/services/interaction_service.py
@@ -0,0 +1,8 @@
+from ..repositories.interaction_repo import InteractionRepository
+
+class InteractionService:
+    def __init__(self, repo: InteractionRepository):
+        self.repo = repo
+
+    def getUserInteractions(self, id: int):
+        return self.repo.getUserInteractions(id)

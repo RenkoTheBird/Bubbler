b0292356081bbeb47b7a06ff63b0517cbcc5bdda
RenkoTheBird <raindrop.spider@protonmail.com>
2026-06-08 18:06:40 -0400

    Add post view

--- backend/app/repositories/graph_repo.py
+++ backend/app/repositories/graph_repo.py
@@ -5,7 +5,7 @@ class GraphRepository:
         self.pool = pool
     
     # since our current limit of posts per screen is 4,
-    # (not counting the current post), we hardcodde the
+    # (not counting the current post), we hardcode the
     # limit as 4 for now
     async def getNeighbors(self, id: int, limit: int = 4):
         async with self.pool.acquire() as conn:

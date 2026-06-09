965c3f4a4be128c00f181b84057a2cebc1a9fbc0
RenkoTheBird <raindrop.spider@protonmail.com>
2026-06-09 13:13:36 -0400

    Update interaction model

--- backend/app/models/interaction.py
+++ backend/app/models/interaction.py
@@ -5,5 +5,9 @@ class Interaction(BaseModel):
     id: str
     user_id: str
     post_id: str
-    type: str
+    type: str # what they did
     created_at: datetime.datetime
+    topic: str
+    
+    view_time: float
+    liked: bool
